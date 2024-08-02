46e6ea04cf9d910b80fb9e1aa5f9c27f30ccb290
eric6166 <ericnguyen6166@gmail.com>
2024-08-02 20:45:24 +0700

    commit

--- src/main/java/com/eric6166/order/dto/OrderResponse.java
+++ src/main/java/com/eric6166/order/dto/OrderResponse.java
@@ -3,22 +3,16 @@ package com.eric6166.order.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@EqualsAndHashCode(callSuper = false)
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class OrderResponse implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
+public class OrderResponse{
 
     private LocalDateTime orderDate;
     private String username;
